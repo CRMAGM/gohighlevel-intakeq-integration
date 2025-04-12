031a371d3439f4578d6f5a0531ef8398fe86dc86
CRMAGM <christian@aestheticgenerator.com>
2025-04-12 04:44:42 -0400

    Update main.py for Heroku deployment

--- main.py
+++ main.py
@@ -11,10 +11,8 @@ from src.api.webhook_server import create_app
 # Load environment variables
 load_dotenv()
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
-    host = "0.0.0.0"  # Heroku requires this
     port = int(os.environ.get("PORT", 5001))
-    debug = os.getenv("DEBUG", "False").lower() == "true"
-    
-    app.run(host=host, port=port, debug=debug)
+    app.run(host="0.0.0.0", port=port, debug=True)
